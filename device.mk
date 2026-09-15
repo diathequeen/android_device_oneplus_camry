461409886e9367e0f57fa3dd92514dbf42b5765e
DiamondQueen <ghostieqwe@gmail.com>
2026-09-15 20:42:53 +0300

    change vibrator effect library to camry

--- device.mk
+++ device.mk
@@ -44,7 +44,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
-$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus.waffle)
+$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus.camry)
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
