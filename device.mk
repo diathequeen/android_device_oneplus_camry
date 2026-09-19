d306ceb6baae9f6429cbdf5f1f66ae8ab4b8b899
DiamondQueen <ghostieqwe@gmail.com>
2026-09-19 22:26:43 +0300

    camry: inherit common trees

--- device.mk
+++ device.mk
@@ -51,5 +51,8 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
+# Inherit from the common OEM chipset makefile.
+$(call inherit-product, device/oneplus/sm6375-common/common.mk)
+
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/camry/camry-vendor.mk)
